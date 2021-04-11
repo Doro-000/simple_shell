70b38bbdcd11fbeb11f75aeb88c3ee1984ab0636
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-04-12 01:23:32 +0300

    Update helpers3.c

--- helpers3.c
+++ helpers3.c
@@ -2,40 +2,40 @@
 
 /**
  * _strtok_r - tokenizes a string
- * @s: string to be tokenized
+ * @string: string to be tokenized
  * @delim: delimiter to be used to tokenize the string
  * @save_ptr: pointer to be used to keep track of the next token
  *
  * Return: The next available token
  */
-char *_strtok_r (char *s, const char *delim, char **save_ptr)
+char *_strtok_r (char *string, const char *delim, char **save_ptr)
 {
 	char *end;
 
-	if (s == NULL)
-		s = *save_ptr;
+	if (string == NULL)
+		string = *save_ptr;
 
-	if (*s == '\0')
+	if (*string == '\0')
 	{
 		*save_ptr = s;
 		return NULL;
 	}
 
-	s += strspn (s, delim);
-	if (*s == '\0')
+	string += strspn (string, delim);
+	if (*string == '\0')
 	{
-		*save_ptr = s;
+		*save_ptr = string;
 		return NULL;
 	}
 
-	end = s + strcspn (s, delim);
+	end = string + strcspn (string, delim);
 	if (*end == '\0')
 	{
 		*save_ptr = end;
-		return s;
+		return string;
 	}
 
 	*end = '\0';
 	*save_ptr = end + 1;
-	return s;
+	return string;
 }
