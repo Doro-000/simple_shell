5ac473b12c30a06984ffc97881b18199722fa406
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-04-11 03:34:37 +0300

    added check_path
    updated execute_command and parse_command

--- utils.c
+++ utils.c
@@ -5,8 +5,9 @@
  * @command: command to be parsed
  * 
  * Return: constant representing the type of the command
- * Description - EXTERNAL_COMMAND (1) represents commands like ls, /bin/ls
+ * Description - EXTERNAL_COMMAND (1) represents commands like /bin/ls
  * 		 INTERNAL_COMMAND (2) represents commands like exit, env
+ * 		 PATH_COMMAND (3) represents commands found in the PATH like ls
  *		 INVALID_COMMAND (-1) represents invalid commands
  */
 int parse_command(char *command)
@@ -14,6 +15,7 @@ int parse_command(char *command)
 	int i;
 	/*Add one to the index whenever you add a command*/
 	char *internal_command[3] = {"env", "exit", NULL};
+	char *path = NULL;
 
 	for (i = 0; command[i] != '\0'; i++)
 	{
@@ -24,6 +26,10 @@ int parse_command(char *command)
 	{
 		if (_strcmp(command, internal_command[i]) == 0)
 			return (INTERNAL_COMMAND);
+	}
+	if ((path = check_path(command)) != NULL)
+	{
+		return (PATH_COMMAND);
 	}
 
 	return (INVALID_COMMAND);
@@ -39,6 +45,7 @@ int parse_command(char *command)
 void execute_command(char **tokenized_command, int command_type)
 {
 	void (*func)(char **command);
+
 	if (command_type == EXTERNAL_COMMAND)
 	{
 		if (execve(tokenized_command[0], tokenized_command, NULL) == -1)
@@ -46,13 +53,38 @@ void execute_command(char **tokenized_command, int command_type)
 			perror("$");
 		}
 	}
-	if (command_type == INTERNAL_COMMAND)
+	else if (command == PATH_COMMAND)
+	{
+		tokenized_command[0] = check_path(tokenized_command[0]);
+		execute_command(tokenized_command, EXTERNAL_COMMAND);
+	}
+	else if (command_type == INTERNAL_COMMAND)
 	{
 		func = get_func(tokenized_command[0]);
 		func(tokenized_command);
 	}
-	if (command == INVALID_COMMAND)
-	{
+	else
 		;
+}
+
+/**
+ * check_path - checks if a command is found in the PATH
+ * @command: command to be used
+ *
+ * Return: path where the command is found in, NULL if not found
+ */
+char *check_path(char *command)
+{
+	char **path_array = tokenizer(_getenv("PATH"), ":");
+	char *temp;
+	int i;
+
+	for (i = 0; path_array[i] != NULL; i++)
+	{
+		temp = _strcat(path_array[i], "/");
+		temp = _strcat(test, command);
+		if (access(temp, F_OK) == 0)
+			return (temp);
 	}
+	return (NULL);
 }
