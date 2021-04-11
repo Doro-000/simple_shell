e2b72243801a91d88a80f2819c49da45ca419886
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-04-11 03:07:09 +0300

    Added _strcat

--- helpers2.c
+++ helpers2.c
@@ -19,3 +19,23 @@ int _strcmp(char *first, char *second)
 	}
 	return first[i] - second[i];
 }
+
+/**
+ * _strcat - concatenates 2 strings
+ * @destination: string to be concatenated to
+ * @source: string to concatenate
+ *
+ * Return: address of the new_string
+ */
+char *_strcat(char *destination, char *source)
+{
+	char *new_string = NULL; 
+	int len_dest = _strlen(destination);
+	int len_source = _strlen(source);
+
+	new_string = malloc(sizeof(*new_string) * (len_dest + len_source + 1));
+	_strcpy(destination, new_string);
+	_strcpy(source, new_string + len_dest);
+	new_string[len_dest + len_source] = '\0';
+	return (new_string);
+}
