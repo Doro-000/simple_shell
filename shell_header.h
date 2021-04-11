00269a2024c544a478def3e944a68bfb19280e0f
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-04-11 03:35:02 +0300

    Update shell_header.h

--- shell_header.h
+++ shell_header.h
@@ -14,7 +14,8 @@
 /*Constants*/
 #define EXTERNAL_COMMAND 1
 #define INTERNAL_COMMAND 2
-#define SPECIAL_COMMAND 3
+#define PATH_COMMAND 3
+#define INVALID_COMMAND -1
 
 /*helpers*/
 void print(char *);
@@ -25,5 +26,11 @@ void _strcpy(char *, char*);
 
 /*helpers2*/
 int _strcmp(char *, char *);
+char *_strcat(char *, char *);
+
+/*utils*/
+int parse_command(char *);
+void execute_command(char **, int);
+char *check_path(char *);
 
 #endif /*SHELL_H*/
