16687f56ded804eda3106479449ad11643221d3e
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-04-13 12:34:05 +0300

    Added _atoi

--- helpers3.c
+++ helpers3.c
@@ -39,3 +39,25 @@ char *_strtok_r (char *string, char *delim, char **save_ptr)
 	*save_ptr = end + 1;
 	return string;
 }
+
+/**
+ * _atoi - changes a string to an int
+ * @s: the string to be changed
+ *
+ * Return: the converted int
+ */
+int _atoi(char *s)
+{
+	int i = 1;
+	unsigned int n = 0;
+
+	do {
+		if (*s == '-')
+			i *= -1;
+		else if (*s >= '0'  && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	} while (*s++);
+	return (n * i);
+}
