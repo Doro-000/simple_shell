fb30d04439c5ff5c92729204ca7610a8ba731f04
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-04-13 12:44:03 +0300

    added quit

--- built_in.c
+++ built_in.c
@@ -8,12 +8,35 @@
  * Return: void
  * Description - tokenized_command is used here to make the function
  		comaptible for use in the function_map type, and hence is 
-		ignored
+		ignored because env doesn't use it
  */
 void env(char **tokenized_command __attribute__((unused)))
 {
 	int i;
 
 	for (i = 0; environ[i] != NULL; i++)
+	{
 		print(environ[i]);
+		print("\n");
+	}
+}
+
+/**
+ * quit - exits the shell
+ * @tokenized_command - command entered
+ *
+ * Return: void
+ */
+void quit(char **tokenized_command)	
+{
+	int num_token = 0;
+
+	for (tokenized_command[num_token] != NULL; num_token++)
+		;
+	if (num_token == 1)
+		exit(EXIT_SUCCESS);
+	else if (num_token == 2)
+		exit(_atoi(tokenized_command[1]));
+	else
+		print("$: exit doesn't take more than one argument");
 }
