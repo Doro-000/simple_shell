b08fbe25b7345794e136ece7cb2aebb65a9729f3
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-04-14 21:33:54 +0300

    Added _strdup

--- helpers3.c
+++ helpers3.c
@@ -61,3 +61,30 @@ int _atoi(char *s)
 	} while (*s++);
 	return (n * i);
 }
+
+/**
+ * _strdup - allocates space in memory, which contains a copy of the str
+ * @str: pointer to the string to be copied to the allocated memory
+ *
+ * Return: pointer to the memory block
+ */
+char *_strdup(char *str)
+{
+	char *block;
+	int i = 0, size = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[size])
+		size++;
+	block = malloc(sizeof(char) * size + 1);
+	if (block != NULL)
+	{
+		for (; i <= size; i++)
+			block[i] = str[i];
+		block[i] = '\0';
+	}
+	else
+		return (NULL);
+	return (block);
+}
