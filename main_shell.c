0d52217d94ae3553ce1b050eb6088fd68db8fe14
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-14 22:41:13 +0300

    Update main_shell.c

--- main_shell.c
+++ main_shell.c
@@ -1,3 +1,4 @@
+  
 #include "shell_header.h"
 
 /**
@@ -11,9 +12,8 @@ int main(int argc, char **argv)
 {
 	char **commands = NULL, **current_command = NULL;
 	char *line = NULL;
+	int i, type_command = 0;
 	size_t n = 0;
-	int type_command, i;
-	pid_t child;
 
 	if (argc > 1)
 	{
@@ -21,16 +21,7 @@ int main(int argc, char **argv)
 		{
 			current_command = tokenizer(argv[i], " ");
 			type_command = parse_command(current_command[0]);
-			if (type_command == EXTERNAL_COMMAND || type_command == PATH_COMMAND)
-			{
-				child = fork();
-				if (child == 0)
-					execute_command(current_command, type_command);
-				else
-					wait(NULL);
-			}
-			else
-				execute_command(current_command, type_command);
+			initalizer(current_command, type_command);
 			free(current_command);
 		}
 		return (0);
@@ -48,20 +39,35 @@ int main(int argc, char **argv)
 		{
 			current_command = tokenizer(commands[i], " ");
 			type_command = parse_command(current_command[0]);
-			if (type_command == EXTERNAL_COMMAND || type_command == PATH_COMMAND)
-			{
-				child = fork();
-				if (child == 0)
-					execute_command(current_command, type_command);
-				else
-					wait(NULL);
-			}
-			else
-				execute_command(current_command, type_command);
+			initalizer(current_command, type_command);
 			free(current_command);
 		}
 		free(commands);
 	}
 	free(line);
 	return (0);
+}
+
+/**
+ * initalizer - starts exectuting everything xD
+ * @current_command: try to check current token
+ * @type_command: parse token
+ *
+ * Return: void function
+ */
+
+void initalizer(char **current_command, int type_command)
+{
+	pid_t child;
+
+	if (type_command == EXTERNAL_COMMAND || type_command == PATH_COMMAND)
+	{
+		child = fork();
+		if (child == 0)
+			execute_command(current_command, type_command);
+		else
+			wait(NULL);
+	}
+	else
+		execute_command(current_command, type_command);
 }
