6be53944385bb8baedbad0f8f3bf34be9b36a00d
Doro-000 <hizkyastekle@gmail.com>
2021-04-14 07:57:47 +0000

    Handled non interactive mode, needs to be shorter

--- main_shell.c
+++ main_shell.c
@@ -4,19 +4,38 @@
  * main - entry point, the main shell
  * @argc: number of arguments passed to the shell
  * @argv: list of arguments passed to the shell
- * @env: list of environment variables available to the shell
  *
  * Return: 0 on success
  */
-int main()
+int main(int argc, char **argv)
 {
 	char **commands = NULL;
 	char **current_command = NULL;
 	char *line = NULL;
 	size_t n = 0;
-	int i, type_command;
+	int i = 1, type_command;
 	pid_t child;
 
+	if (argc > 1)
+	{
+		while (argv[i] != NULL)
+		{
+			current_command = tokenizer(argv[i], " ");
+			type_command = parse_command(current_command[0]);
+			if (type_command == EXTERNAL_COMMAND || type_command == PATH_COMMAND)
+			{
+				child = fork();
+				if (child == 0)
+					execute_command(current_command, type_command);
+				else
+					wait(NULL);
+			}
+			else
+				execute_command(current_command, type_command);
+			i++;
+		}
+		return (0);
+	}
 	while (1)
 	{
 		print("$ ");
