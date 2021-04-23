b3d35286f67f693a5fc7742c31cfd092f333d2b1
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-04-23 18:54:17 +0300

    Update and rename env_ignore_ls.bash to path_path1_var.bash

--- path_path1_var.bash
+++ path_path1_var.bash
@@ -2,7 +2,7 @@
 
 ################################################################################
 # Description for the intranet check (one line, support Markdown syntax)
-# Remove all environment variables and execute `ls`
+# Remove PATH variable and set a `PATH1` variable, and execute `ls`
 
 ################################################################################
 # The variable 'compare_with_sh' IS OPTIONNAL
@@ -21,7 +21,7 @@
 # as follows: "echo $shell_input | ./hsh"
 #
 # It can be empty and multiline
-shell_input="ls"
+shell_input="hbtn_ls"
 
 ################################################################################
 # The variable 'shell_params' IS OPTIONNAL
@@ -41,15 +41,10 @@ shell_input="ls"
 # Return value: Discarded
 function check_setup()
 {
-	current_env=$(/usr/bin/env)
-	for i in `/usr/bin/env | /usr/bin/cut -d'=' -f1`
-	do
-		unset $i
-	done
-
-	# Important: Disable valgrind when running without an environment
-	let valgrind_error=0
-	let valgrind_leak=0
+	cp "/bin/ls" "$PWD/hbtn_ls"
+	export PATH1="$PWD"
+	OLDPATH="$PATH"
+	unset PATH
 
 	return 0
 }
@@ -90,13 +85,10 @@ function sh_setup()
 #     1  -> Check fails
 function check_callback()
 {
-	let status=0
+	status=$1
 
-	$ECHO -n "" > $EXPECTED_OUTPUTFILE
-	$ECHO "./hsh: 1: ls: not found" > $EXPECTED_ERROR_OUTPUTFILE
-	$ECHO -n "127" > $EXPECTED_STATUS
-
-	check_diff
+	export PATH="$OLDPATH"
+	$RM -f "$PWD/hbtn_ls"
 
 	return $status
 }
