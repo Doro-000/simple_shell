f24111a04ba1376c57df73a60932c44b817500e7
Doro-000 <hizkyastekle@gmail.com>
2021-04-24 13:01:41 +0000

    fixed bug on remove_comment

--- helpers3.c
+++ helpers3.c
@@ -125,9 +125,11 @@ void remove_comment(char *input)
 {
 	int i = 0;
 
+	if (input[i] == '#')
+		input[i] = '\0';
 	while (input[i] != '\0')
 	{
-		if (input[i] == '#')
+		if (input[i] == '#' && input[i - 1] == ' ')
 			break;
 		i++;
 	}
