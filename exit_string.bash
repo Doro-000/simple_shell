296cb020e7ab4e6d382384e02e098a079bc3fa37
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-04-24 12:12:04 +0300

    Update and rename exit_neg_number.bash to exit_string.bash

--- exit_string.bash
+++ exit_string.bash
@@ -2,7 +2,7 @@
 
 ################################################################################
 # Description for the intranet check (one line, support Markdown syntax)
-# Exit with a negative number
+# Exit with a string as parameter
 
 ################################################################################
 # The variable 'compare_with_sh' IS OPTIONNAL
@@ -21,7 +21,7 @@
 # as follows: "echo $shell_input | ./hsh"
 #
 # It can be empty and multiline
-shell_input="exit -98"
+shell_input="exit HBTN"
 
 ################################################################################
 # The variable 'shell_params' IS OPTIONNAL
